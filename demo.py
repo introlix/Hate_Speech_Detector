a9ef911d6d888231339a01b078b669a7e31c6fd6
Satyam Mishra <tubex998@gmail.com>
2024-02-29 14:23:55 +0545

    setup

---
+++ demo.py
@@ -0,0 +1,12 @@
+import sys
+from hate.logger import logging
+from hate.exception import CustomException
+
+def devide(a,b):
+    try:
+        logging.info(f"Dividing {a} by {b}")
+        return a/b
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+devide(2, 0)
