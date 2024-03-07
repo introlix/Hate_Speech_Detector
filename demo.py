019887f3c2d1bbd81d60ef13bb4d8fbfdfd8310a
Introlix <apibrains@gmail.com>
2024-03-07 21:56:30 +0545

    gcloud connected

--- demo.py
+++ demo.py
@@ -1,12 +1,7 @@
 import sys
 from hate.logger import logging
 from hate.exception import CustomException
+from hate.configuation.gcloud_syncer import GCloudSync
 
-def devide(a,b):
-    try:
-        logging.info(f"Dividing {a} by {b}")
-        return a/b
-    except Exception as e:
-        raise CustomException(e, sys)
-    
-devide(5, 0)
+obj = GCloudSync()
+obj.sync_folder_from_gcloud("hatespeech-15d3d.appspot.com", "dataset.zip", "dataset")
